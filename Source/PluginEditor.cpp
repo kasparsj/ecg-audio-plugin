eaa4e25e26d4e45e26e186acfbeaf9bf772ba989
Kaspars Jaudzems <kasparsj@gmail.com>
2022-09-27 14:17:34 +0300

    SimpleBLE added

--- Source/PluginEditor.cpp
+++ Source/PluginEditor.cpp
@@ -9,6 +9,17 @@ AudioPluginAudioProcessorEditor::AudioPluginAudioProcessorEditor (AudioPluginAud
     // Make sure that before the constructor has finished, you've set the
     // editor's size to whatever you need it to be.
     setSize (400, 300);
+
+    if (!SimpleBLE::Adapter::bluetooth_enabled()) {
+        std::cout << "Bluetooth is not enabled" << std::endl;
+        return;
+    }
+
+    addAndMakeVisible(scanButton);
+    scanButton.setButtonText ("Scan");
+    scanButton.onClick = [this]() { startScan(); };
+
+    adapters = SimpleBLE::Adapter::get_adapters();
 }
 
 AudioPluginAudioProcessorEditor::~AudioPluginAudioProcessorEditor()
@@ -23,11 +34,39 @@ void AudioPluginAudioProcessorEditor::paint (juce::Graphics& g)
 
     g.setColour (juce::Colours::white);
     g.setFont (15.0f);
-    g.drawFittedText ("Hello World!", getLocalBounds(), juce::Justification::centred, 1);
+
+    juce::String text = "";
+    if (adapters.empty()) {
+        text = "No Bluetooth adapters found";
+    }
+    else {
+        for (int i=0; i<adapters.size(); i++) {
+            text += juce::String("Adapter identifier: " + adapters[i].identifier() + " address: " + adapters[i].address() + "\r\n");
+        }
+        for (auto peripheral : peripherals) {
+            text += juce::String("Peripheral identifier: " + peripheral.identifier() + " address: " + peripheral.address() + "\r\n");
+        }
+    }
+    g.drawFittedText (text, getLocalBounds(), juce::Justification::centred, 4);
 }
 
 void AudioPluginAudioProcessorEditor::resized()
 {
-    // This is generally where you'll want to lay out the positions of any
-    // subcomponents in your editor..
+    auto area = getLocalBounds();
+    area.reduce (40, 30);
+    scanButton.setBounds (area.removeFromTop (35).reduced (100, 0));
+}
+
+void AudioPluginAudioProcessorEditor::startScan()
+{
+    // Get the first adapter
+    SimpleBLE::Adapter adapter = adapters[0];
+
+    // Scan for peripherals for 5000 milliseconds
+    adapter.scan_for(5000);
+
+    // Get the list of peripherals found
+    peripherals = adapter.scan_get_results();
+
+    repaint();
 }
--- Source/PluginEditor.h
+++ Source/PluginEditor.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "PluginProcessor.h"
+#include <simpleble/SimpleBLE.h>
 
 //==============================================================================
 class AudioPluginAudioProcessorEditor  : public juce::AudioProcessorEditor
@@ -13,10 +14,16 @@ public:
     void paint (juce::Graphics&) override;
     void resized() override;
 
+    void startScan();
+
 private:
     // This reference is provided as a quick way for your editor to
     // access the processor object that created it.
     AudioPluginAudioProcessor& processorRef;
+
+    juce::TextButton scanButton;
+    std::vector<SimpleBLE::Adapter> adapters;
+    std::vector<SimpleBLE::Peripheral> peripherals;
 
     JUCE_DECLARE_NON_COPYABLE_WITH_LEAK_DETECTOR (AudioPluginAudioProcessorEditor)
 };
